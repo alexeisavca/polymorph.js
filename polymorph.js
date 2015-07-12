7146bcf5b4e703cd65377bfac9db437031a90cb6
Samuel Rouse <samuel.rouse@gmail.com>
2015-07-12 09:53:10 -0400

    Cleanup
    Added semicolons.
    Consistent spacing for if and for.
    Added curly braces to single statement if/else.
    Removed parentheses from typeof and delete.
    Added strict equality.
    Simplified isEqualParams.
    Moved delete into the conditional, to eliminate the variable and
    additional conditional.

--- polymorph.js
+++ polymorph.js
@@ -22,52 +22,66 @@ License: MIT [ http://www.opensource.org/licenses/mit-license.php ]
  */
 
 function polymorph() {
+
 	var getParameterTypes = function(types, func) {
 		var param = func.toString().match(/\((.*?)\)/)[1].split(/,\s*/);
-		for(var i = 0; i < param.length; i++) param[i] = types[param[i]];
+		for (var i = 0; i < param.length; i++) {
+            param[i] = types[param[i]];
+        }
 		return param;
-	}
+	};
+
 	var clone = function(obj) {
 		var newObj = (obj instanceof Array) ? [] : {};
 		for (i in obj) {
-			if (obj[i] && typeof obj[i] == "object")
-				newObj[i] = clone(obj[i])
-			else
-				newObj[i] = obj[i];
+			if (obj[i] && typeof obj[i] === "object") {
+                newObj[i] = clone(obj[i]);
+            } else {
+                newObj[i] = obj[i];
+            }
 		}
 		return newObj;
-	}
+	};
+
 	var isEqualParams = function(x,y) {
-		if(x.length != y.length) return false;
-		for(var i=0; i<x.length; i++) {
-			if(x[i]==undefined && y[i]==undefined) continue;
-			if(x[i]==y[i]) continue;
-			return false;
+		if (x.length !== y.length) {
+            return false;
+        }
+		for (var i=0; i<x.length; i++) {
+			if (x[i] !== undefined && x[i] !== y[i]) {
+                return false;
+            }
 		}
 		return true;
-	}
+	};
+
 	var addFunctions = function(funcmap, args) {
-		for(var i=0; i<args.length; i++) {
+		for (var i=0; i < args.length; i++) {
 			var params;
-			if(typeof(args[i]) == "object" && i<args.length-1 && typeof(args[i+1]) == "function") {
+
+			if (typeof args[i] === "object" && i<args.length-1 && typeof args[i+1] === "function") {
 				params = getParameterTypes(args[i], args[i+1]);
 				i++;
-			}
-			else if(typeof(args[i]) == "function") {
-				params = getParameterTypes(new Object(), args[i]);
+			} else if (typeof args[i] === "function") {
+				params = getParameterTypes({}, args[i]);
 			}
 			var nparam = args[i].length;
-			if(funcmap[nparam] == undefined) funcmap[nparam] = [];
+
+			if (funcmap[nparam] === undefined) {
+                funcmap[nparam] = [];
+            }
+
 			// Replace existing function with the same prototype if any
 			for(var j=0; j<funcmap[nparam].length; j++) {
-				if(isEqualParams(funcmap[nparam][j][0], params)) {
+				if (isEqualParams(funcmap[nparam][j][0], params)) {
 					funcmap[nparam][j][1] = args[i];
 				}
 			}
 			// Create new one
 			funcmap[nparam].push([params, args[i]]);
 		}
-	}
+	};
+
 	var getPolymorphFunction = function(funcmap) {
 		var res = function() {
 			var argLen = arguments.length;
@@ -77,8 +91,10 @@ function polymorph() {
 			 * Now includes a check that __fmaplit[argLen] is not undefined
 			 * We could handle the error here, but it will be caught, below
 			 */
-			if (arguments.callee.__fmaplite && arguments.callee.__fmaplite[argLen]) 
-				return arguments.callee.__fmaplite[argLen].apply(this, arguments);
+			if (arguments.callee.__fmaplite && arguments.callee.__fmaplite[argLen]) {
+                return arguments.callee.__fmaplite[argLen].apply(this, arguments);
+            }
+
 			var flist = arguments.callee.__fmap[argLen];
 			
 			// If we don't have the correct number of arguments, stop here.
@@ -91,13 +107,16 @@ function polymorph() {
 				for(var j=0; j<flist[i].length; j++) {
 					var argType = flist[i][0][j];
 					
-					if(argType == undefined || arguments[j] == undefined || (arguments[j] instanceof argType)
-						|| typeof arguments[j] == (argType.name || "").toLowerCase())
-						continue;
+					if (argType === undefined || arguments[j] === undefined || (arguments[j] instanceof argType)
+						|| typeof arguments[j] === (argType.name || "").toLowerCase()) {
+                        continue;
+                    }
 					flag = false;
 					break;
 				}
-				if(flag) return flist[i][1].apply(this,arguments);
+				if (flag) {
+                    return flist[i][1].apply(this,arguments);
+                }
 			}
 			return flist[i][1].apply(this,arguments);
 			/*
@@ -106,16 +125,16 @@ function polymorph() {
 			Should not harm anything
 			*/
 			this.__base;
-		}
+		};
 
 		var flag = true;
 		for(var i in funcmap) {
-			if(funcmap[i].length>1) {
+			if (funcmap[i].length > 1) {
 				flag = false;
 				break;
 			}
 		}
-		if(flag) {
+		if (flag) {
 			res.__fmaplite = [];
 			for(var i in funcmap) {
 				res.__fmaplite[i] = funcmap[i][0][1];
@@ -124,25 +143,27 @@ function polymorph() {
 		res.__fmap = funcmap;
 		res.update = function() {
 			addFunctions(this.__fmap, arguments);
-			if(this.__fmaplite != undefined) {
-				// Flag is "only one function for each argument length"
-				var flag = true;
-				for(var i in this.__fmap) {
-					if(this.__fmap[i].length>1) {
-						flag = false;
+
+			if (this.__fmaplite !== undefined) {
+				// Only one function for each arity?
+				for (var i in this.__fmap) {
+                    if (this.__fmap[i].length > 1) {
+                        // Can't have __fmapLite
+                        delete this.__fmaplite;
 						break;
 					}
 				}
-				if(!flag) delete(this.__fmaplite);
 			}
-		}
+		};
+
 		res.inherit = function() {
 			var funcmap = clone(this.__fmap);
 			addFunctions(funcmap, arguments);
 			return getPolymorphFunction(funcmap);
-		}
+		};
 		return res;
-	}
+	};
+
 	var funcmap = [];
 	addFunctions(funcmap, arguments);
 	return getPolymorphFunction(funcmap);
